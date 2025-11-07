08a410e91df4080af4546508f2a3b4c3927349f9
Jennifer Sucso <jhenisucso@gmail.com>
2025-11-07 01:25:54 +0000

    feat: insertar notas

---
+++ my-notes-app/src/App.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+interface Note {
+ id: number;
+ title: string;
+ body: string;
+}
+function App() {
+ const [notes, setNotes] = useState<Note[]>([]);
+const [title, setTitle] = useState("");
+const [body, setBody] = useState("");
+const handleSubmit = (e: React.FormEvent) => {
+ e.preventDefault();
+ const newNote: Note = {
+ id: notes.length + 1,
+ title,
+ body,
+ };
+ setNotes([newNote, ...notes]);
+ setTitle("");
+ setBody("");
+}
+ // useEffect se ejecuta cuando carga la app
+ useEffect(() => {
+ fetch("https://jsonplaceholder.typicode.com/posts?_limit=5")
+ .then((res) => res.json())
+ .then((data) => setNotes(data));
+ }, []);
+ return (
+ <div style={{ padding: "20px" }}>
+    <h1>Mini Notes App SENATI</h1>
+    <form onSubmit={handleSubmit}>
+        <div>
+            <label>Añadir Nueva nota</label> <br />
+            <input type="text" 
+            placeholder="Titulo"
+            value={title} 
+            style={{ marginTop: "1rem" }}/> <br />
+
+            <input type="text" 
+            placeholder="Contenido" 
+            value={body} 
+            style={{ marginTop: "1rem" }}/>
+        </div>
+        <button type="submit" style={{ marginTop: "1rem" }}>Enviar</button>
+    </form>
+    <ul>
+    {notes.map((note) => (
+    <li key={note.id}>
+    <strong>{note.title}</strong>
+    <p>{note.body}</p>
+    </li>
+    ))}
+    </ul>
+ </div>
+ );
+}
+export default App;
